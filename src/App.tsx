7ba78a4c905bf79e019ad8760fe112f27f237090
eIonut <67823306+eIonut@users.noreply.github.com>
2023-06-29 17:06:51 +0300

    Refactor - simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import NavBar from './components/NavBar'
 import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
 import { useState } from 'react'
-import { Genre } from './hooks/useGenres'
 import PlatformSelector from './components/PlatformSelector'
 import { Platform } from './hooks/usePlatforms'
 import SortSelector from './components/SortSelector'
 import GameHeading from './components/GameHeading'
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -36,7 +35,7 @@ function App() {
     </GridItem>
     <Show above="lg">
       <GridItem area="aside" paddingX={5}>
-        <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})}></GenreList>
+        <GenreList selectedGenreId={gameQuery.genreId} onSelectGenre={(genre) => setGameQuery({...gameQuery, genreId: genre.id})}></GenreList>
       </GridItem>
     </Show>
     <GridItem area="main">
@@ -44,7 +43,7 @@ function App() {
       <GameHeading gameQuery={gameQuery}></GameHeading>
       <Flex marginBottom={5}>
         <Box marginRight={5}>
-          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}></PlatformSelector>
+          <PlatformSelector selectedPlatformId={gameQuery.platformId} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platformId: platform.id})}></PlatformSelector>
         </Box>
         <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}></SortSelector>
       </Flex>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,10 +4,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if(error) return null;
@@ -21,7 +21,7 @@ const GenreList = ({selectedGenre, onSelectGenre }: Props) => {
       <ListItem paddingY="5px" key={genre.id}>
         <HStack>
           <Image boxSize="32px" objectFit='cover' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}></Image>
-          <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
+          <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
         </HStack>
       </ListItem>)}</List>
     </>

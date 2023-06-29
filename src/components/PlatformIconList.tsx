a5252e1dae4ca56e74be75de298448fe18091ecf
eIonut <67823306+eIonut@users.noreply.github.com>
2023-06-29 12:46:17 +0300

    Refactoring - remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa';
-import { Platform } from '../hooks/useGames';
+import { Platform } from '../hooks/usePlatforms';
 import { HStack, Icon } from '@chakra-ui/react';
 import { MdPhoneIphone } from 'react-icons/md';
 import { SiNintendo } from 'react-icons/si';
